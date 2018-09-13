8bbfa5a5d931c59e0335aefca2adf40c02dda880
okay <okay@chalk>
2018-09-13 10:58:03 -0700

    dont use inputs

--- src/components/ReactCrossword/ReactCrossword.jsx
+++ src/components/ReactCrossword/ReactCrossword.jsx
@@ -1,4 +1,4 @@
-var Button = require('../Button/Button');
+var Button = require('Button/Button');
 var List = require('./List');
 var React = require("vendor/react");
 
@@ -6,7 +6,7 @@ var cssUtils = require("common/css_utils");
 var WaitForCss = cssUtils.WaitForCss;
 var ScopedCss = cssUtils.ScopedCss;
 
-default class MyComponent extends React.Component{
+export default class MyComponent extends React.Component{
 
   constructor(props){
     super(props);
@@ -28,45 +28,68 @@ default class MyComponent extends React.Component{
       isDown,
     } = this.state;
 
-    this.cell_changed(j, i);
+
+
+    this.setState({ x: i, y: j });
     // switch direction if clicking on the same square
     if (j == y && i == x) {
       this.setState({ isDown: !this.state.isDown });
     }
-    e.target.setSelectionRange(0, 10);
   }
 
-  onKeydown(j, i, e) {
-    // TODO: detect backspace and move forward
-  }
+  onKeyDown(e) {
+    var s = String.fromCharCode(e.keyCode);
+    var isLetter = s.match(/[a-z]/i);
 
-  onChanged(j, i, e) {
-    const { board } = this.state;
-    // only can have one letter at a time in the board
-    let val = e.target.value;
-    if (val) {
-      val = val[val.length-1];
-    } else {
-      val = "";
+    console.log("PRESSED",e.keyCode);
+
+    // keycode 8 = backspace
+    // keycode 9 = tab
+    // keycode 65 to 128 is characters
+    // keycode 33 to 65 is symbols or something
+    var inc = 1;
+    var character;
+    // TODO: arrow keys should navigate around without entering character
+    if (e.keyCode < 65) {
+      character = '#';
+      if (e.keyCode == 8) {
+        character = '_';
+        inc = -1;
+      } else if (e.keyCode == 9) {
+        this.state.isDown = !this.state.isDown;
+        e.preventDefault();
+        this.forceUpdate();
+        return;
+      } else if (e.keyCode < 30) {
+        return;
+      }
+
+    } else if (isLetter) {
+      character = s;
     }
 
-    board[j][i] = val;
-    this.setState({ board });
-    e.target.value = val;
+    if (!character) {
+      return;
+    }
+
+    this.state.board[this.state.y][this.state.x] = character;
 
     if (this.state.isDown)  {
-      this.state.x += 1;
+      this.state.x += inc;
     } else {
-      this.state.y += 1;
+      this.state.y += inc;
     }
 
-  }
+    // snap state to board perimeter
+    if (this.state.x < 0) { this.state.x = 0; }
+    if (this.state.y < 0) { this.state.y = 0; }
 
-  cell_changed(y, x) {
-    this.setState({
-      x: x,
-      y: y
-    });
+    var board = this.state.board;
+    if (this.state.x >= board[0].length) { this.state.x = board[0].length - 1; }
+    if (this.state.y >= board.length) { this.state.y = board.length - 1; }
+
+    this.forceUpdate();
+
   }
 
   getSuggestions() {
@@ -79,9 +102,6 @@ default class MyComponent extends React.Component{
 
   }
 
-  toggleSquare() {
-
-  }
   getCellClass(j, i, cellVal) {
     const { x, y, isDown } = this.state;
     let classes = 'cell';
@@ -108,12 +128,10 @@ default class MyComponent extends React.Component{
         <div className='row' key={j}>
           {
             row.map((cell, i) => {
-              return <input type='text'
+              return <span type='text'
                 key={i}
                 className={ this.getCellClass(j, i, board[j][i]) }
-                onKeydown={(e) => { this.onKeydown(j, i, e) }}
-                onChange={(e) => { this.onChanged(j, i, e) }}
-                onClick={(e) => { this.onClicked(j, i, e) }} value={board[j][i]}/ >
+                onClick={(e) => { this.onClicked(j, i, e) }}> {board[j][i]} </span>
             })
           }
         </div>
@@ -122,7 +140,7 @@ default class MyComponent extends React.Component{
 
     return (
       <div className={WaitForCss("Button")}>
-        <div className='crossword'>
+        <div className='crossword noselect' onKeyDown={(e) => { this.onKeyDown(e) }} tabIndex="0" >
             { rows }
         </div>
         <Button className={ScopedCss("Button")}
